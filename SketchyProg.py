243988cec8175b0a52ee227293c2524f504db7ab
Herman Schmit <herman.schmit@gmail.com>
2015-03-26 22:21:23 -0700

    tweak to push

--- SketchyProg.py
+++ SketchyProg.py
@@ -18,6 +18,7 @@ def main(ifile_name, ofile_name1, ofile_name2, bin_fn="bfile.bin", levels=4):
     print sketch.quant_idx
 
 
+
 if __name__ == "__main__":
     if len(sys.argv) == 4:
         main(sys.argv[1], sys.argv[2], sys.argv[3])
